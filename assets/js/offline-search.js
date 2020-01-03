7b0b62c0639fcb04623fe6a2dd3d2aea325f11aa
Orfeas Zafeiris <orfeaz@gmail.com>
2020-01-03 05:56:32 +0200

    Implement api and full text search

--- assets/js/offline-search.js
+++ assets/js/offline-search.js
@@ -1,152 +1,170 @@
 (function ($) {
   'use strict';
 
-  window.renderSearch = function (indexURL) {
+  function searchFulltext(data, query) {
     const $searchResults = $('#search-results');
-    const $apiResults = $('#api-results');
-
-    const params = new URLSearchParams(window.location.search);
-    const searchQuery = params.get('q');
-    const searchQueryLower = searchQuery.toLowerCase();
-
-    // Download the index and search it.
-    let idx = null; // Lunr index
     const resultDetails = new Map();
 
-    $.ajax('/json/vext-api.json').then((apiData) => {
-      var options = {
-        shouldSort: true,
-        matchAllTokens: true,
-        includeMatches: true,
-        threshold: 0.1,
-        location: 0,
-        distance: 100,
-        maxPatternLength: 32,
-        minMatchCharLength: 2,
-        keys: [
-          "fields",
-          "methods"
-        ]
-      };
+    // Create the index.
+    const idx = lunr(function() {
+      this.ref('ref');
+      this.field('title', { boost: 100 });
+      this.field('body');
 
-      var fuse = new Fuse(apiData, options);
-      var results = fuse.search(searchQuery, { limit: 15 });
+      data.forEach((doc) => {
+        this.add(doc);
 
-      for (const result of results) {
-        let matches = [];
+        resultDetails.set(doc.ref, {
+          title: doc.title,
+          excerpt: doc.excerpt
+        });
+      });
+    });
 
-        for (const match of result.matches) {
-          if (match.value.toLowerCase().indexOf(searchQueryLower) === -1) {
-            continue;
-          }
+    // Search the index and limit to the first 100 results.
+    const results = idx.query((q) => {
+      const tokens = lunr.tokenizer(query);
 
-          let matchText = result.item.name;
+      tokens.forEach(token => {
+        const queryString = token.toString();
 
-          if (match.key === 'fields') {
-            matchText += '.';
-          } else {
-            matchText += ':';
-          }
+        q.term(queryString, {
+          boost: 100
+        });
 
-          matchText += match.value;
+        q.term(queryString, {
+          wildcard:
+            lunr.Query.wildcard.LEADING |
+            lunr.Query.wildcard.TRAILING,
+          boost: 10
+        });
 
-          matches.push(matchText);
-        }
+        q.term(queryString, {
+          editDistance: 2
+        });
+      });
+    }).slice(0, 100);
 
-        if (matches.length === 0) {
+    results.forEach((r) => {
+      const doc = resultDetails.get(r.ref);
+
+      const $resultTitle = $('<strong>');
+      const $resultText = $('<span>');
+
+      $resultTitle.append($('<a>').attr('href', r.ref).text(doc.title));
+      $resultText.text(doc.excerpt);
+
+      const $searchResult = $('<div>').addClass('search-result').append($resultTitle).append($resultText);
+
+      $searchResults.append($searchResult);
+    });
+
+    return results.length;
+  }
+
+  function searchApi(data, query) {
+    const $apiResults = $('#api-results');
+
+    var options = {
+      shouldSort: true,
+      matchAllTokens: true,
+      includeMatches: true,
+      threshold: 0.1,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 2,
+      keys: [
+        "fields",
+        "methods"
+      ]
+    };
+
+    var fuse = new Fuse(data, options);
+    var results = fuse.search(query, { limit: 30 });
+
+    let resultCount = 0;
+
+    for (const result of results) {
+      for (const match of result.matches) {
+        if (match.value.toLowerCase().indexOf(query) === -1) {
           continue;
         }
 
-        console.log('<div>' + matches.join('<br/>') + '</div>');
+        let matchText = result.item.name;
+        let matchUrl = '/' + result.item.id + '#';
 
-        $apiResults.append($('<div>' + matches.join('<br/>') + '</div>'));
+        if (match.key === 'fields') {
+          matchText += '.';
+          matchUrl += 'properties'
+        } else {
+          matchText += ':';
+          matchUrl += 'methods'
+        }
+
+        matchText += match.value;
+
+        const $resultTitle = $('<strong>');
+        const $resultText = $('<span>');
+
+        $resultTitle.append($('<a>').attr('href', '/' + result.item.id).text(result.item.name));
+        $resultText.append($('<a>').attr('href', matchUrl).text(matchText));
+
+        const $apiResult = $('<div>').addClass('api-result').append($resultTitle).append($resultText);
+
+        $apiResults.append($apiResult);
+        ++resultCount;
+      }
+    }
+
+    return resultCount;
+  }
+
+  function performSearch(apiIdx, fulltextIdx, query) {
+    const apiResults = searchApi(apiIdx, query);
+    const fulltextResults = searchFulltext(fulltextIdx, query);
+
+    if (apiResults === 0 && fulltextResults === 0) {
+      $('#search-status').text(`No results found for query "${query}".`);
+    } else {
+      $('#search-status').text(`Found ${apiResults + fulltextResults} results for query "${query}".`);
+    }
+
+    if (apiResults > 0) {
+      $('#api-results').prepend($('<h3>').text('API Reference Results'));
+    }
+
+    if (fulltextResults > 0) {
+      $('#search-results').prepend($('<h3>').text('Full-text Results'));
+    }
+  }
+
+  window.renderSearch = function(fulltextIndexUrl, apiIndexUrl) {
+    const params = new URLSearchParams(window.location.search);
+    const searchQuery = params.get('q') || '';
+    const searchQueryLower = searchQuery.toLowerCase().trim();
+
+    if (searchQueryLower.length < 2) {
+      $('#search-status').text('Please enter a search query of 2 or more characters.');
+      return;
+    }
+
+    let downloadedFulltext = null;
+    let downloadedApi = null;
+
+    $.ajax(fulltextIndexUrl).then((data) => {
+      downloadedFulltext = data;
+
+      if (downloadedApi !== null) {
+        performSearch(downloadedApi, downloadedFulltext, searchQueryLower);
       }
     });
 
-    $.ajax(indexURL).then((data) => {
-      const idx = lunr(function () {
-        this.ref('ref');
-        this.field('title', { boost: 2 });
-        this.field('body');
+    $.ajax(apiIndexUrl).then((data) => {
+      downloadedApi = data;
 
-        data.forEach(doc => {
-          this.add(doc);
-
-          resultDetails.set(doc.ref, {
-            title: doc.title,
-            excerpt: doc.excerpt
-          });
-        });
-      });
-
-      const results = idx.query(q => {
-        const tokens = lunr.tokenizer(searchQuery.toLowerCase());
-        tokens.forEach(token => {
-          const queryString = token.toString();
-          q.term(queryString, {
-            boost: 100
-          });
-          q.term(queryString, {
-            wildcard:
-              lunr.Query.wildcard.LEADING |
-              lunr.Query.wildcard.TRAILING,
-            boost: 10
-          });
-          q.term(queryString, {
-            editDistance: 2
-          });
-        });
-      }).slice(0, 50);
-
-      //
-      // Make result html
-      //
-
-      $searchResults.html('');
-      const $html = $searchResults;
-
-      $html.append(
-        $('<div>')
-          .css({
-            display: 'flex',
-            justifyContent: 'space-between',
-            marginBottom: '1em'
-          })
-      );
-
-      const $searchResultBody = $('<div>').css({
-        overflowY: 'auto'
-      });
-
-      $html.append($searchResultBody);
-
-      if (results.length === 0) {
-        $searchResultBody.append(
-          $('<p>').text(`No results found for query "${searchQuery}"`)
-        );
-      } else {
-        results.forEach(r => {
-          const $cardHeader = $('<div>').addClass('card-header');
-          const doc = resultDetails.get(r.ref);
-
-          $cardHeader.append(
-            $('<a>')
-              .attr('href', r.ref)
-              .text(doc.title)
-          );
-
-          const $cardBody = $('<div>').addClass('card-body');
-          $cardBody.append(
-            $('<p>')
-              .addClass('card-text text-muted')
-              .text(doc.excerpt)
-          );
-
-          const $card = $('<div>').addClass('card');
-          $card.append($cardHeader).append($cardBody);
-
-          $searchResultBody.append($card);
-        });
+      if (downloadedFulltext !== null) {
+        performSearch(downloadedApi, downloadedFulltext, searchQueryLower);
       }
     });
   };
